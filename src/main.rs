a14836c0bcc83855a332456658e308901c2d8e1f
Oleh Bohzok <olehbozhok@gmail.com>
2024-08-12 23:18:41 +0300

    add exit codes

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 #[deny(clippy::unwrap_used)]
+use std::process::ExitCode;
+
 mod app_config;
 use app_config::get_config;
 
 mod app;
 
-fn main() {
+fn main() -> ExitCode {
     stderrlog::new().module(module_path!()).init().unwrap();
 
     let config = get_config();
 
     if let Err(err) = app::App::new(config).start_handle() {
-        log::error!("error: {err}")
+        log::error!("error: {err}");
+        ExitCode::FAILURE
+    } else {
+        ExitCode::SUCCESS
     }
 }
